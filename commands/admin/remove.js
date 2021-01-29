c61e03e4f43b2735fcb1a69986e568db10466ecf
Nate Goldsborough <business@playantares.com>
2021-01-29 16:41:15 -0600

    new commands

---
+++ commands/admin/remove.js
@@ -0,0 +1,21 @@
+const { Command } = require('discord.js-commando');
+const onReady = require('../../actions/onReady.js');
+//require('colors');
+
+module.exports = class RemoveCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'remove',
+            group: 'admin',
+            memberName: 'remove',
+            description: 'Removes information about the server from the database',
+            examples: ['remove'],
+            guildOnly: true
+        });
+    }
+
+    run(message) {
+        message.channel.send("If you would like to request that all your data be removed from our servers, please DM @nathen418#0002");
+        //logToConsole.command(message.guild, message);
+    }
+};
