251817e7c110938f04503597d718089c346294be
Nate Goldsborough <business@playantares.com>
2021-01-28 19:28:31 -0600

    updates

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ require('dotenv').config();
 require('colors');
 const path = require('path');
 const onReady = require('./actions/onReady');
+const docCreate = require('./actions/docCreate');
+const guildModel = require('./models/guild');
+const piiModel = require('./models/pii');
+const piiCreate = require('./actions/piiCreate');
+const gateModel = require('./models/gate');
 
 const bot = new CommandoClient({
     commandPrefix: '*',
@@ -32,7 +37,56 @@ bot.on('ready', async () => {
 	console.log("Startup script has run".red.bold)
 });
 
+//actions to run when the bot joins a server
+bot.on("guildCreate", async (guild) => {
+	docCreate.event(guild, bot);
+	piiCreate.event(guild, bot);
 
+})
+
+//actions to run when the bot leaves a server
+bot.on("guildDelete", async (guild) => {
+	var d = new Date();
+	try {
+		await guildModel.findOneAndUpdate({ GUILD_ID: guild.id }, { $set: { GUILD_LEAVE_DATE: d.toString() } }, { new: true });
+		bot.users.fetch('603629606154666024', false).then((user) => {
+			user.send(`I left a server :(\n Name: ${guild.name}\n ID: ${guild.id}\n Owner: ${guild.owner}`);
+		});
+	} catch (e) {
+		console.log(e);
+	}
+})
+
+bot.on('guildMemberAdd', async (member) => {
+	var memberList = [];
+	bot.guilds.cache.get(member.guild.id).members.cache.forEach(member => memberList.push(member.user.id));
+	console.log(memberList)
+	try {
+		await piiModel.findOneAndUpdate({ GUILD_ID: member.guild.id }, { $set: { GUILD_MEMBERS: memberList } }, { new: true });
+		console.log(`MEMBER JOIN`.teal, `[${member.guild.name}]`.green,  `[${member.username}]`.yellow)
+	} catch (e) {
+		console.log(e);
+	}
+})
+
+bot.on('guildMemberRemove', async (member) => {
+	var memberList = [];
+	bot.guilds.cache.get(member.guild.id).members.cache.forEach(member => memberList.push(member.user.id));
+	console.log(memberList)
+	try {
+		await piiModel.findOneAndUpdate({ GUILD_ID: member.guild.id }, { $set: { GUILD_MEMBERS: memberList } }, { new: true });
+		console.log(`MEMBER LEAVE`.yellow, `[${member.guild.name}]`.green,  `[${member.username}]`.yellow)
+	} catch (e) {
+		console.log(e);
+	}
+})
+bot.on("error", (e) => console.error(e));
+bot.on("warn", (e) => console.warn(e));
+
+
+bot.on('messageDelete', async (message) => {
+	console.log(`DELETE`.red, `[${message.guild.name}]`.green, `[${message.channel.name}]`.blue, `[${message.author.username}]`.yellow, `--`.grey, `${message.content}`.red)
+});
 //connect to MongoDB and then log bot into Discord
 (async () => {
 	var mongo_uri = String(process.env.BOT_MONGO_PATH);
