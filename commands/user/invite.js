338259ae179cd4042c27b540c31b32a213b0a134
Nate Goldsborough <business@playantares.com>
2021-02-03 10:37:10 -0600

    force send in default channel

--- commands/user/invite.js
+++ commands/user/invite.js
@@ -1,4 +1,5 @@
 const { Command } = require('discord.js-commando');
+const channelCheck = require('../../functions/channelCheck')
 
 module.exports = class InviteCommand extends Command {
     constructor(client) {
@@ -12,12 +13,14 @@ module.exports = class InviteCommand extends Command {
         });
     }
 
-    run(message) {
-        message.channel.send("https://discord.com/oauth2/authorize?client_id=736086156759924762&scope=bot&permissions=8");
-        message.author.send("Thank you for inviting me to your server, " +
-        "\n If you would like to join our support/comunity server, click the link below:" + 
-        "\n https://discord.gg/6pZ2wtGANP");
-        //message.channel.send("https://discord.gg//6pZ2wtGANP")
-        //logToConsole.command(message.guild, message);
+    async run(message) {
+        if (await channelCheck.check(message) == true) {
+            message.channel.send("https://discord.com/oauth2/authorize?client_id=736086156759924762&scope=bot&permissions=8");
+            message.author.send("Thank you for inviting me to your server, " +
+                "\n If you would like to join our support/comunity server, click the link below:" +
+                "\n https://discord.gg/6pZ2wtGANP");
+            //message.channel.send("https://discord.gg//6pZ2wtGANP")
+            //logToConsole.command(message.guild, message);
+        }
     }
 };
--- commands/user/xkcd.js
+++ commands/user/xkcd.js
@@ -1,18 +1,19 @@
 const { Command } = require('discord.js-commando');
 const { MessageEmbed } = require('discord.js');
 const fetch = require('node-fetch');
+const channelCheck = require('../../functions/channelCheck')
 
 
 
 function embed(message, img, title) {
     const Embed = new MessageEmbed()
-            .setColor('#ff3505')
-            .setURL('https://discord.gg/6pZ2wtGANP')
-            .setTitle(title)
-            .setImage(img)
-            .setFooter(`Delivered in: ${Date.now() - message.createdTimestamp}ms | Antares Bot | ${botVersion}`, 'https://cdn.discordapp.com/icons/649703068799336454/1a7ef8f706cd60d62547d2c7dc08d6f0.png');
-        message.channel.send(Embed);
-        //logToConsole.command(message.guild, message);
+        .setColor('#ff3505')
+        .setURL('https://discord.gg/6pZ2wtGANP')
+        .setTitle(title)
+        .setImage(img)
+        .setFooter(`Delivered in: ${Date.now() - message.createdTimestamp}ms | Antares Bot | ${botVersion}`, 'https://cdn.discordapp.com/icons/649703068799336454/1a7ef8f706cd60d62547d2c7dc08d6f0.png');
+    message.channel.send(Embed);
+    //logToConsole.command(message.guild, message);
 }
 
 module.exports = class XkcdCommand extends Command {
@@ -28,19 +29,20 @@ module.exports = class XkcdCommand extends Command {
         });
     }
     async run(message) {
-
-        let comicNum = Math.floor(Math.random() * 2413);
-        try {
-        var url = `http://xkcd.com/${comicNum}/info.0.json`;
+        if (await channelCheck.check(message) == true) {
+            let comicNum = Math.floor(Math.random() * 2413);
+            try {
+                var url = `http://xkcd.com/${comicNum}/info.0.json`;
                 //request a comic from the url
                 fetch(url)
-                .then(res => res.json())
-                .then(json => embed(message, json.img, `Random Comic from XKCD`));
-        } catch (e) {
-            message.channel.send("Error. Please try again.");
-            console.error(e)
-        }
+                    .then(res => res.json())
+                    .then(json => embed(message, json.img, `Random Comic from XKCD`));
+            } catch (e) {
+                message.channel.send("Error. Please try again.");
+                console.error(e)
+            }
             //send to the console that this command was run
             //logToConsole.command(message.guild, message);
+        }
     }
 };
