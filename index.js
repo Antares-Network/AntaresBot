9df2bac9bcdcc878e994a9f1fc79cbbfbc43df76
Nate Goldsborough <business@playantares.com>
2021-02-03 21:16:18 -0600

    Update version to 1.3.0-RC3

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const guildModel = require('./models/guild');
 const piiModel = require('./models/pii');
 const piiCreate = require('./actions/piiCreate');
 const counting = require('./functions/counting');
-global.botVersion = "1.3.0-RC2";
+global.botVersion = "1.3.0-RC3";
 
 
 global.bot = new CommandoClient({
