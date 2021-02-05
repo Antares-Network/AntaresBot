7fa319df63640d43fbe45e08edb41ce3f51f4c4e
Nate Goldsborough <business@playantares.com>
2021-02-05 16:39:56 -0600

    show bot stats on startup

--- actions/onReady.js
+++ actions/onReady.js
@@ -7,14 +7,19 @@
 //connecting to databases, etc
 const guildModel = require('../models/guild');
 const piiModel = require('../models/pii');
-const docCreate = require('../actions/docCreate');
+const docCreate = require('./docCreate');
 const piiCreate = require('./piiCreate');
+const gateCreate = require('./gateCreate');
+const gateModel = require('../models/gate');
+
 
 module.exports = {
-	event: function (bot) {
+	event: async function (bot) {
+		const gate = await gateModel.findOne({ NAME: 'GATE' }); //find the entry for the guild
 		bot.guilds.cache.forEach(async guild => {
 			const doc = await guildModel.findOne({ GUILD_ID: guild.id }); //find the entry for the guild
 			const req = await piiModel.findOne({ GUILD_ID: guild.id }); //find the entry for the guild
+			const gate = await gateModel.findOne({ NAME: 'GATE' }); //find the entry for the guild
 			if (doc === null) {
 				docCreate.event(guild, bot);
 				console.log('Made new doccument'.yellow);
@@ -24,9 +29,16 @@ module.exports = {
 				console.log("Created PII doc".yellow);
 			}
 		});
+		if (gate == null) {
+			gateCreate.event(bot);
+			console.error("NO GATE FOUND. CLOSING BOT.")
+		}
 		bot.user.setActivity(`&help | V: ${botVersion}`, { type: 'PLAYING' });
 		console.log(`Set bot status to:`, `&help`.magenta, `| V:`, `${botVersion}`.magenta);
 		console.log(`Logged in as`, `${bot.user.tag}`.magenta);
 		console.log("The bot is online.".green);
+		console.log(`I am in`.yellow, `${gate.TOTAL_SERVERS}`.green, ` servers`.yellow)
+		console.log(`I am being used by`.yellow, `${gate.TOTAL_USERS}`.green, `users`.yellow)
+		console.log(`The bot is ready...`.red.bold)
 	}
 };
