359a49db34b64990d89faf91cae40c56f74ae660
Nate Goldsborough <business@playantares.com>
2021-02-11 10:10:32 -0600

    remove join/leave logging

--- actions/logToConsole.js
+++ actions/logToConsole.js
@@ -22,20 +22,10 @@ module.exports = {
 
     memberJoin: async function (member) {
 		console.log(`MEMBER JOIN`.blue, `[${member.guild.name}]`.green, `[${member.user.username}]`.yellow)
-        try {
-            bot.channels.cache.get(process.env.REPORTING_CHANNEL).send(`**MEMBER JOIN** - [${member.guild.name}] [${member.user.username}]`)
-        } catch (e) {
-            console.log(e);
-        }
     },
 
     memberLeave: async function (member) {
 		console.log(`MEMBER LEAVE`.blue, `[${member.guild.name}]`.green, `[${member.user.username}]`.yellow)
-        try {
-            bot.channels.cache.get(process.env.REPORTING_CHANNEL).send(`**MEMBER LEAVE** - [${member.guild.name}] [${member.user.username}]`)
-        } catch (e) {
-            console.log(e);
-        }
     },
 
     guildUpdate: async function (oldGuild, newGuild) {
