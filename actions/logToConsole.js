fcfef4c2f1d526466a68e1e5321aed4b3223e79a
Nate Goldsborough <business@playantares.com>
2021-02-12 21:39:38 -0600

    removed command log storage

--- actions/logToConsole.js
+++ actions/logToConsole.js
@@ -3,12 +3,12 @@ const messageLog = require("./messageLog")
 module.exports = {
     command: async function (guild, message) {
         console.log(`COMMAND`.red, `[${guild.name}]`.green, `[${message.channel.name}]`.blue, `[${message.author.username}]`.yellow, `--`.grey, `${message.content}`.cyan)
-        try {
-            bot.channels.cache.get(process.env.REPORTING_CHANNEL).send(`**COMMAND** - [${guild.name}] [${message.channel.name}] [${message.author.username}] -- **${message.content}**`)
+        // try {
+        //     bot.channels.cache.get(process.env.REPORTING_CHANNEL).send(`**COMMAND** - [${guild.name}] [${message.channel.name}] [${message.author.username}] -- **${message.content}**`)
 
-        } catch (e) {
-            console.log(e);
-        }
+        // } catch (e) {
+        //     console.log(e);
+        // }
         messageLog.log(message);
     },
 
