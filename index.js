6e8bf11fda33094a09825a957e36bb5a6f1d0d7e
Nate Goldsborough <business@playantares.com>
2021-03-30 15:52:57 -0500

    Update version to 1.3.18

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const guildUpdate = require('./actions/guildUpdate')
 global.config = require('./config.json');
 require('dotenv').config();
 require('colors');
-global.botVersion = "1.3.17";
+global.botVersion = "1.3.18";
 
 
 global.bot = new CommandoClient({
