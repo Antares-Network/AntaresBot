a01a4137d9a104431435e03a4f8f1851ad05d27c
Nate Goldsborough <business@playantares.com>
2021-04-11 14:12:00 -0500

    Improve promise rejection handling

--- commands/user/cat.js
+++ commands/user/cat.js
@@ -12,7 +12,7 @@ function embed(message, img, title) {
         //.setURL('https://dsc.gg/antaresnetwork')
         .setTitle(title)
         .setImage(img)
-        .setFooter(`Delivered in: ${this.client.ws.ping}ms | Antares Bot | ${botVersion}`, 'https://cdn.discordapp.com/icons/649703068799336454/1a7ef8f706cd60d62547d2c7dc08d6f0.png');
+        .setFooter(`Delivered in: ${bot.ws.ping}ms | Antares Bot | ${botVersion}`, 'https://cdn.discordapp.com/icons/649703068799336454/1a7ef8f706cd60d62547d2c7dc08d6f0.png');
     message.channel.send(Embed);
     //logToConsole.command(message.guild, message);
 }
@@ -35,10 +35,21 @@ module.exports = class CatCommand extends Command {
         if (await channelCheck.check(message) == true) {
             //request a cat from the api
             fetch('http://aws.random.cat/meow')
-                .then(res => res.json())
-                .then(json => embed(message, json.file, 'Random Cat Picture'));
+                .then((response) => {
+                    if (response.status >= 200 && response.status <= 299) {
+                        return response.json();
+                    } else {
+                        throw Error(response.statusText);
+                    }
+                })
+                .then((json) => {
+                    embed(message, json.file, 'Random Cat Picture')
+                }).catch((error) => {
+                    // Handle the error
+                    message.channel.send(`**\`Err:\`** Socket hang up. Please try again.`);
+                    console.log(error);
+                });
         }
-
         //send to the console that this command was run
         logToConsole.command(message.guild, message);
     }
