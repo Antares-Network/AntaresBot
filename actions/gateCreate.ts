ba028c513bc2ba7a114bab840d2fbde7270a4d50
Nate Goldsborough <nathen418@playantares.com>
2021-09-22 00:23:12 -0500

    DB logic

---
+++ actions/gateCreate.ts
@@ -0,0 +1,41 @@
+import { Client } from 'discord.js'
+import gateModel from './../models/gate'
+import guildModel from './../models/guild'
+
+
+
+async function event(client: Client) {
+
+        //var init and gc
+        var totalUsers = 0;
+        var totalMessages = 0;
+        var totalOwners: any[] = [];
+
+        //loop thru the guilds and add the owners, member numbers and messages to their vars to be added to the db 
+        client.guilds.cache.forEach(async guild => {
+            const doc = await guildModel.findOne({ GUILD_ID: guild.id }); //find the doc that has all the guild information in it
+            totalMessages += Number(doc.GUILD_MESSAGES);
+            totalOwners.push(guild.ownerId);
+            totalUsers += guild.memberCount
+        });
+
+        //make it pause 5 seconds before saving to the database because then the stuff above has time to finish processing
+        setTimeout(async () => {
+            const gate = new gateModel({
+                NAME: 'GATE',
+                GUILD_OWNER_ID: totalOwners,
+                BANNED_OWNERS: [],
+                BANNED_USERS: [],
+                BANNED_GUILDS: [],
+                TOTAL_MESSAGES: totalMessages,
+                TOTAL_SERVERS: client.guilds.cache.size,
+                TOTAL_USERS: totalUsers,
+                IGNORED_GUILDS: null,
+                UPDATE_TIME: null
+            });
+            //upload db file to the db remote server
+            await gate.save();
+        }, 5000);
+}
+
+export = { event };
