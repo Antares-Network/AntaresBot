b3b5bdcd06d36a3cf7a7aef6457f61d46596f836
Nate Goldsborough <nathen418@playantares.com>
2021-09-28 00:48:14 -0500

    oops

--- functions/channelCheck.ts
+++ functions/channelCheck.ts
@@ -45,20 +45,6 @@ async function check(message: Message, client: Client): Promise<boolean> {
             return Promise.resolve(false);
         }
     }
-    //check if admin commands were sent in the admin channel
-    if (message.channel.id != srv.GUILD_ADMIN_CHANNEL) {
-        if (srv.GUILD_ADMIN_CHANNEL == null) {
-            message.channel.send("The server owner has not set an admin channel yet.\n If you are the server owner please use `&admin #channel`");
-            return Promise.resolve(false); //exit the loop and don't parse the command
-        } else {
-            //ping the admin in the admin channel
-            let MSG = await (client.channels?.cache.get(srv.GUILD_ADMIN_CHANNEL) as TextChannel).send(`<@${message.author.id}> Please use admin commands in this channel`);
-            setTimeout( async () => {
-                MSG.delete()
-            }, 5000);
-            return Promise.resolve(false);
-        }
-    }
     return Promise.resolve(true);
 }
 
