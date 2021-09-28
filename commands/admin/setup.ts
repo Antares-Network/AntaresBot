f3a17ba45d87e92e162fd4ce9e39e47829612278
Nate Goldsborough <nathen418@playantares.com>
2021-09-27 23:41:37 -0500

    add setup command

---
+++ commands/admin/setup.ts
@@ -0,0 +1,30 @@
+import { MessageEmbed } from 'discord.js';
+import { ICommand } from "wokcommands";
+import piiModel from  '../../models/pii';
+
+
+export default {
+    category: 'admin',
+    description: 'Setup command for the default bot channel',
+    aliases: ['defaultChannel'],
+    slash: false,
+    testOnly: true,
+    guildOnly: true,
+    permissions: ['MANAGE_GUILD'],
+
+    callback: async ({ client ,channel, message }) => {
+        if (message.mentions.channels.first() === undefined) {
+            channel.send("Please re-run this command and mention a channel as an argument.")
+        } else {
+            //check to see if a default channel is set for this server yet
+            const req = await piiModel.findOne({ GUILD_ID: message.guild?.id });
+            //if the server has a default channel, send it here
+            if (req.DEFAULT_CHANNEL_ID !== null) {
+                channel.send(`This server's default channel was: <#${req.GUILD_DEFAULT_CHANNEL}>`);
+            }
+            const doc = await piiModel.findOneAndUpdate({ GUILD_ID: message.guild?.id }, { $set: { GUILD_DEFAULT_CHANNEL: message.mentions.channels.first()?.id } }, { new: true });
+            message.channel.send(`Set the default channel to <#${doc.GUILD_DEFAULT_CHANNEL}>`);
+            await doc.save();
+        }
+    }
+} as ICommand
