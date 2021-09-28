5edfdd96453e82cca24ce4cd2ddc1e8b6921f16f
Nate Goldsborough <nathen418@playantares.com>
2021-09-28 00:48:36 -0500

    remove GUILD_MEMBERS as a needed intent

--- index.ts
+++ index.ts
@@ -24,12 +24,12 @@ dotenv.config();
 const client = new DiscordJs.Client({
     intents: [
         Intents.FLAGS.GUILDS,
-		Intents.FLAGS.GUILD_MEMBERS,
+		//Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
 		Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
 		Intents.FLAGS.DIRECT_MESSAGES,
-		Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+		Intents.FLAGS.DIRECT_MESSAGE_REACTIONS
     ]
 });
 
