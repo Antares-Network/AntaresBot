eedf08d2b97bb3dbf92008bc6aa6e8159b22e18c
Nate Goldsborough <nathen418@playantares.com>
2021-12-08 23:18:17 -0600

    revamped channel checking

--- functions/channelCheck.ts
+++ functions/channelCheck.ts
@@ -1,7 +1,5 @@
 import {
-  Channel,
   User,
-  Message,
   CommandInteraction,
   MessageEmbed,
   Client,
@@ -13,36 +11,30 @@ import gateModel from "../models/gate";
 async function check( interaction: CommandInteraction, channel: TextChannel, author: User, client: Client ): Promise<boolean> {
   const srv = await piiModel.findOne({ GUILD_ID: channel.guild?.id }); //find the entry for the guild
   const gate = await gateModel.findOne({ NAME: "GATE" });
+
+  // check if the user has been banned from using the bot
+  if (gate.BANNED_GUILDS.includes(interaction.guild?.id)) {
+    const banEmbed = new MessageEmbed()
+      .setColor("#ff3505")
+      .setTitle("Guild ban notice")
+      .setDescription(
+        `<@${interaction.user.id}>, This guild has been banned from using this bot by the owner.`
+      );
+      interaction.reply({embeds: [banEmbed], ephemeral: true});
+    return Promise.resolve(false);
+  }
+
   //check if the user has been banned from using the bot
-  // if (gate.BANNED_GUILDS.includes(message.guild?.id)) {
-  //   message.delete();
-  //   const banEmbed = new MessageEmbed()
-  //     .setColor("#ff3505")
-  //     .setTitle("Guild ban notice")
-  //     .setDescription(
-  //       `<@${message.author.id}>, This guild has been banned from using this bot by the owner.`
-  //     );
-  //   const MSG = await message.channel.send({ embeds: [banEmbed] });
-  //   setTimeout(() => {
-  //     MSG.delete();
-  //   }, 5000);
-  //   return Promise.resolve(false);
-  // }
-  // //check if the user has been banned from using the bot
-  // if (gate.BANNED_USERS.includes(message.author.id)) {
-  //   message.delete();
-  //   const banEmbed = new MessageEmbed()
-  //     .setColor("#ff3505")
-  //     .setTitle("User ban notice")
-  //     .setDescription(
-  //       `<@${message.author.id}>, You have been banned from using this bot by the owner.`
-  //     );
-  //   const MSG = await message.channel.send({ embeds: [banEmbed] });
-  //   setTimeout(() => {
-  //     MSG.delete();
-  //   }, 5000);
-  //   return Promise.resolve(false);
-  // }
+  if (gate.BANNED_USERS.includes(interaction.user.id)) {
+    const banEmbed = new MessageEmbed()
+      .setColor("#ff3505")
+      .setTitle("User ban notice")
+      .setDescription(
+        `<@${interaction.user.id}>, You have been banned from using this bot by the owner.`
+      );
+      interaction.reply({ embeds: [banEmbed], ephemeral: true });
+    return Promise.resolve(false);
+  }
 
   //check if the user sent their message in the default channel
   if (channel.id != srv.GUILD_DEFAULT_CHANNEL) {
