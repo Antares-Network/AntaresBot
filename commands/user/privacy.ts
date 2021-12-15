e3f766aadbef13c6d8a360adf40547fa6e3ad539
Nate Goldsborough <nathen418@playantares.com>
2021-12-14 23:33:13 -0600

    Updated privacy policy

--- commands/user/privacy.ts
+++ commands/user/privacy.ts
@@ -15,11 +15,15 @@ export default {
       "**Data Collected By Command and when features are enabled**\n" +
         "The following may be collected when the bot joins a server and or when a user voluntarily enters this information. " +
         "When providing data in this way, you forego any rights to the content of the data provided.\n" +
-        "Server configurations (name, id, age, icon,)\n" +
-        "Server ownership\n" +
-        "Messages sent (these are **not** stored but the bot sees them) *messages sent in the bot support server* **are** *stored*\n" +
-        //"Server invites (The bot may create an invite for your server)\n" +
-        "Number of commands and messages sent\n\n" +
+        "**Bot Join Date**" +
+        "**Guild Name**" +
+        "**Guild Owner ID**" +
+        "**Guild Icon URL**" +
+        "**The total messages sent in the guild**" +
+        "**Guild Member count**" +
+        "**Guild prefix**" +
+        "**Some channel id’s**(Admin channel, Default channel, music channel, counting channel, etc)" +
+        "**The current number in the counting channel**" +
         "**All data is stored on secured servers. Maximum efforts are taken to keep collected data protected, but absolute security cannot be guaranteed. We are not liable for any damages or stolen information, in which we collect, from our servers.**\n\n" +
         "**Server Member Agreement**\n" +
         "By being a member of a server which uses Antares Bot's services or features, you are consenting to the policies outlined in this agreement. If you, the server member, do not agree with any policies outlined in this agreement, you have the right to leave the server in which this bot is a part of.\n\n" +
@@ -28,7 +32,6 @@ export default {
         "This policy may change at any time without warning. It is your responsibility as a server owner to keep up with the changes.\n\n" +
         `**To request the data we store on you to be deleted from our database, please run the command ${process.env.BOT_DEFAULT_PREFIX}remove**\n\n`
     );
-    //"However the following data will remain: **Server Name, ID, Owner, and previous bot join date**")
     return "You have been sent a DM with the privacy policy. If you do not have DM's enabled, you will not get this message.";
   },
 } as ICommand;
