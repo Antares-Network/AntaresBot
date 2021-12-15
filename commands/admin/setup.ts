26ec52f3163ff22f876947dc0be625bf18eb2c71
Nate Goldsborough <nathen418@playantares.com>
2021-12-14 23:32:03 -0600

    Merged setup, admin setup, and added music setup

--- commands/admin/setup.ts
+++ commands/admin/setup.ts
@@ -11,6 +11,12 @@ export default {
 	permissions: ["MANAGE_GUILD", "MANAGE_CHANNELS"],
 	options: [
 		{
+			name: "type",
+			description: "default, admin, music",
+			type: DiscordJS.Constants.ApplicationCommandOptionTypes.STRING,
+			required: true,
+		},
+		{
 			name: "channel",
 			description: "The channel to setup",
 			type: DiscordJS.Constants.ApplicationCommandOptionTypes.CHANNEL,
@@ -18,16 +24,9 @@ export default {
 		},
 	],
 
-	callback: async ({ channel, interaction, args }) => {
-		if (args[0] === undefined) {
-			channel.send("Please re-run this command and mention a channel as an argument.");
-		} else {
-			//check to see if a default channel is set for this server yet
-			const req = await piiModel.findOne({ GUILD_ID: interaction.guild?.id });
-			//if the server has a default channel, send it here
-			if (req.DEFAULT_CHANNEL_ID !== null) {
-				interaction.reply(`This server's default channel was: <#${req.GUILD_DEFAULT_CHANNEL}>`);
-			}
+	callback: async ({ interaction, args }) => {
+		const req = await piiModel.findOne({ GUILD_ID: interaction.guild?.id });
+		if (args[0].toLowerCase() === "default") {
 			const doc = await piiModel.findOneAndUpdate(
 				{ GUILD_ID: interaction.guild?.id },
 				{
@@ -39,6 +38,28 @@ export default {
 			);
 			interaction.reply(`Set the default channel to <#${doc.GUILD_DEFAULT_CHANNEL}>`);
 			await doc.save();
+		} else if (args[0].toLowerCase() === "admin") {
+			const doc = await piiModel.findOneAndUpdate(
+				{ GUILD_ID: interaction.guild?.id },
+				{
+					$set: { GUILD_ADMIN_CHANNEL: interaction.options.getChannel("channel") },
+				},
+				{ new: true }
+			);
+			interaction.reply(`Set the admin channel to <#${doc.GUILD_ADMIN_CHANNEL}>`);
+			await doc.save();
+		} else if (args[0].toLowerCase() === "music") {
+			const doc = await piiModel.findOneAndUpdate(
+				{ GUILD_ID: interaction.guild?.id },
+				{
+					$set: { GUILD_MUSIC_CHANNEL: interaction.options.getChannel("channel") },
+				},
+				{ new: true }
+			);
+			interaction.reply(`Set the music channel to <#${doc.GUILD_MUSIC_CHANNEL}>`);
+			await doc.save();
+        } else {
+			interaction.reply("Invalid type.\nYou need to specify either default, admin, or music as the channel type to setup with this command.");
 		}
 	},
 } as ICommand;
