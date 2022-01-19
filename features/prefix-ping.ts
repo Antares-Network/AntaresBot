64b969f9bd90707b619ba7e2fd387098a4b85ebd
Nate Goldsborough <nathen418@playantares.com>
2022-01-19 13:39:36 -0600

    oops

--- features/prefix-ping.ts
+++ features/prefix-ping.ts
@@ -7,7 +7,7 @@ export default (client: Client, instance: WOKCommands) => {
 		if (message.author.bot) return;
 		if (message.content === `<@!${client.user?.id}>`) {
 			const prefix = instance.getPrefix(message.guild) || instance.defaultPrefix;
-			message.reply(`My prefix in this server is ${prefix}\n **ALL USER COMMANDS HAVE NOW BEEN MIGRATED TO SLASH COMMANDS**\n Learn how slash commands work [here](https://support.discord.com/hc/en-us/articles/1500000368501-Slash-Commands-FAQ)`);
+			message.reply(`My prefix in this server is ${prefix}\n **ALL USER COMMANDS HAVE NOW BEEN MIGRATED TO SLASH COMMANDS**\n Learn how slash commands work here: https://support.discord.com/hc/en-us/articles/1500000368501-Slash-Commands-FAQ`);
 		}
 	});
 };
