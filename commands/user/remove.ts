ef35deeb980b504bd82f34f956609ce0fa9193c6
Nate Goldsborough <nathen418@playantares.com>
2022-01-20 16:52:54 -0600

    make ephemeral

--- commands/user/remove.ts
+++ commands/user/remove.ts
@@ -1,16 +1,17 @@
 import { ICommand } from "wokcommands";
 
 export default {
-  category: "user",
-  description: "Request deletion of all data from our servers.",
-  slash: true,
-  testOnly: false,
-  guildOnly: false,
-  permissions: ["SEND_MESSAGES"],
+	category: "user",
+	description: "Request deletion of all data from our servers.",
+	slash: true,
+	testOnly: false,
+	guildOnly: false,
+	permissions: ["SEND_MESSAGES"],
 
-  callback: async ({ interaction }) => {
-    interaction.reply(
-      "No per user data is stored on our servers.\nIf you are a server owner and would like your data deleted, kick the bot from your server as all data is deleted upon the bot leaving the server."
-    );
-  },
+	callback: async ({ interaction }) => {
+		interaction.reply({
+			content: "No per user data is stored on our servers.\nIf you are a server owner and would like your data deleted, kick the bot from your server as all data is deleted upon the bot leaving the server.",
+			ephemeral: true,
+		});
+	},
 } as ICommand;
