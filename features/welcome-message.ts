960c6461095da9bd0537100162f0e40e0c85d667
Nate Goldsborough <nathen418@playantares.com>
2022-01-24 14:25:56 -0600

    patch out welcome message for now

--- features/welcome-message.ts
+++ features/welcome-message.ts
@@ -3,23 +3,24 @@ import piiModel from "../models/pii";
 
 export default (client: Client) => {
 	client.on("guildMemberAdd", async (member) => {
-		const res = await piiModel.findOne({ GUILD_NAME: member.guild.name });
-		let welcomeMessage = res?.WELCOME_MESSAGE;
-		if (res.GUILD_WELCOME_ENABLED === false) {
-			return;
-		} else {
-			const channel = member.guild.channels.cache.find(res.GUILD_WELCOME_CHANNEL) as TextChannel;
-			if (!channel) {
-				return;
-			} else {
-				welcomeMessage = welcomeMessage.replace(/{USER}/g, `<@${member.user.username}>`);
-				welcomeMessage = welcomeMessage.replace(/{SERVER_NAME}/g, member.guild.name);
-				welcomeMessage = welcomeMessage.replace(/{NEXT_CHANNEL}/g, `<#${res?.GUILD_WELCOME_NEXT_CHANNEL}>`);
-				channel.send(welcomeMessage).then((msg) => {
-					setTimeout(() => msg.delete(), 1000 * 10);
-				});
-			}
-		}
+
+		// const res = await piiModel.findOne({ GUILD_NAME: member.guild.name });
+		// let welcomeMessage = res?.WELCOME_MESSAGE;
+		// if (res.GUILD_WELCOME_ENABLED === false) {
+		// 	return;
+		// } else {
+		// 	const channel = member.guild.channels.cache.find(res.GUILD_WELCOME_CHANNEL) as TextChannel;
+		// 	if (!channel) {
+		// 		return;
+		// 	} else {
+		// 		welcomeMessage = welcomeMessage.replace(/{USER}/g, `<@${member.user.username}>`);
+		// 		welcomeMessage = welcomeMessage.replace(/{SERVER_NAME}/g, member.guild.name);
+		// 		welcomeMessage = welcomeMessage.replace(/{NEXT_CHANNEL}/g, `<#${res?.GUILD_WELCOME_NEXT_CHANNEL}>`);
+		// 		channel.send(welcomeMessage).then((msg) => {
+		// 			setTimeout(() => msg.delete(), 1000 * 10);
+		// 		});
+		// 	}
+		// }
 	});
 };
 
